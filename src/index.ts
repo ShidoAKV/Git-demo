fa7f8b04c12767507b15052cf5e58dca8870f4d1
abhishek <abhi3vish@gmail.com>
2026-02-07 00:51:47 +0530

    Adding single line

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,1 @@
 const x:number=5;
-console.log(x+6);
-console.log('new code from abhishek/temp');
-const add=(a:number,b:number):number=>{
-    return a+b;
-}
-console.log(add(5,6));
-
-console.log(x*6);
-console.log('branch abhishek/temp2')
