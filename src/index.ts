f2b6e5f10c246c570cb25ca48056a886e9c4b021
abhishek <abhi3vish@gmail.com>
2026-02-07 00:45:45 +0530

    Adding string

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-const x:number=5;
-console.log(x+6);
-console.log('new code from abhishek/temp');
-const add=(a:number,b:number):number=>{
-    return a+b;
-}
-console.log(add(5,6));
-
-console.log(x*6);
+const name:String="abhisehk";
+console.log(name);
